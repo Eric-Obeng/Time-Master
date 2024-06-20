e7c38dcaf837812932f39eccd21e0f5822e6c994
Eric-Obeng <obengeric.3958@gmail.com>
2024-06-20 20:07:27 +0000

    Object-Oriented Clock: A clock object with properties like, hours, minutes, and seconds created

--- script.js
+++ script.js
@@ -6,7 +6,27 @@ const currentHour = currentTime.getHours();
 const currentMinute = currentTime.getMinutes();
 const currentSecond = currentTime.getSeconds();
 
+/*
 console.log(`Current Time: ${currentTime}`);
 console.log(`Hour: ${currentHour}`);
 console.log(`Minute: ${currentMinute}`);
 console.log(`Seconds: ${currentSecond}`);
+*/
+
+function Clock(hours, minutes, seconds) {
+  this.hours = hours;
+  this.minutes = minutes;
+  this.seconds = seconds;
+}
+
+const currentClock = new Clock(
+  currentTime.getHours(),
+  currentTime.getMinutes(),
+  currentTime.getSeconds()
+);
+
+/*
+console.log(`Hours: ${currentClock.hours}`);
+console.log(`Minutes: ${currentClock.minutes}`);
+console.log(`Seconds: ${currentClock.seconds}`);
+*/
