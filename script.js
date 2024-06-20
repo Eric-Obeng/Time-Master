e17db9180a9c63f5562167a3646b7323da8f0c5e
Eric-Obeng <obengeric.3958@gmail.com>
2024-06-20 19:58:15 +0000

    Date object to get current time

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 "use strict";
+
+const currentTime = new Date();
+
+const currentHour = currentTime.getHours();
+const currentMinute = currentTime.getMinutes();
+const currentSecond = currentTime.getSeconds();
+
+console.log(`Current Time: ${currentTime}`);
+console.log(`Hour: ${currentHour}`);
+console.log(`Minute: ${currentMinute}`);
+console.log(`Seconds: ${currentSecond}`);
